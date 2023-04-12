2edb5ecf2a38654d1f1ffac48cacc92b90c7611d
NikolajChristianMoeller <nikolajchristianmoeller@gmail.com>
2023-04-12 09:33:51 +0200

    Kan ændre højden på den første søjle

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 "use strict";
 
+window.addEventListener("load", start);
+
+function start() {
+    console.log("Javascript kører");
+
+    const bars = document.querySelectorAll(".bar");
+    const firstBar = bars[0];
+
+
+    let h = 40;
+    firstBar.style.height = `${h}px`;
+
+}
