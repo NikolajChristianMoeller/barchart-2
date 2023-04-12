3dadf6102fa13b15e25fdd09964199acd30faaf2
NikolajChristianMoeller <nikolajchristianmoeller@gmail.com>
2023-04-12 09:45:12 +0200

    Kan sætte højden på én søjle ud fra værdien i et array

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 
 window.addEventListener("load", start);
 
+const data = [10, 31, 31, 25, 21, 16, 9, 15, 19, 5, 12, 10, 5, 32, 21, 15, 15, 11, 8, 27, 28, 25, 20, 20, 25, 19, 13, 8, 11, 30, 32, 4, 12, 16, 3, 2, 1, 18, 19, 20]
+
+
 function start() {
     console.log("Javascript kører");
 
@@ -9,7 +12,7 @@ function start() {
     const firstBar = bars[0];
 
 
-    let h = 40;
+    let h = data[0] / 32 * 100;
     firstBar.style.height = `${h}px`;
 
 }
